efb3936f0be11748bf8251cbd351fba178ab4a55
JOAO BERNARDELLI <joaobernardelli123@gmail.com>
2023-11-27 18:15:54 -0300

    tela de frete, falta montar o salvar

---
+++ AutoSavePI4/Controllers/FretesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoSavePI4.Controllers
+{
+    public class FretesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CadastrarFretes()
+        {
+            return View();
+        }
+    }
+}
